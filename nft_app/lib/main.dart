6a0baab2e18c96aa51b2e286fda63987cb2438d3
Samuel Twumasi <samueltwumasi1290@gmail.com>
2022-07-14 19:52:16 +0000

    update

--- nft_app/lib/main.dart
+++ nft_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
